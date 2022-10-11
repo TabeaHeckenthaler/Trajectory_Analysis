d14a1c37c8ad72101cf613d9bd72c75735cd8b99
Tabea Heckenthaler <tabea.heckenthaler@weizmann.ac.il>
2022-10-11 13:14:56 +0300

    first commit

--- Directories.py
+++ Directories.py
@@ -1,11 +1,16 @@
 from os import path
+import os
 
-data_home = path.join(path.sep + path.sep + 'phys-guru-cs', 'ants', 'Tabea', 'PyCharm_Data', 'AntsShapes')
+# data_home = path.join(path.sep + path.sep + 'phys-guru-cs', 'ants', 'Tabea', 'PyCharm_Data', 'AntsShapes')
+with open('data_directory.txt') as f:
+    lines = f.readlines()
+data_home = lines[0]
+home = os.getcwd()
+
 PhaseSpaceDirectory = path.join(data_home, 'Configuration_Spaces')
 
-home = path.join(path.abspath(__file__).split('\\')[0] + path.sep, *path.abspath(__file__).split(path.sep)[1:-1])
-
 work_dir = path.join(data_home, 'Pickled_Trajectories')
+
 
 SaverDirectories = {'ant': {True: path.join(work_dir, 'Ant_Trajectories', 'Free'),
                             False: path.join(work_dir, 'Ant_Trajectories', 'Slitted')},
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Analyze the trajectory data
+
+Make sure that the Directory in data_directory.txt
+is the place where your data is saved. 
